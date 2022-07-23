e61b2589d386d3db9516044728791c870c598408
aragonnetje6 <twanstok@gmail.com>
2022-07-23 03:55:27 +0200

    implemented arguments and verbosity

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,46 @@
-use std::env;
+
 use std::fs;
 use std::fs::DirEntry;
+use std::path::Path;
 
+use clap::Parser;
 use regex::Regex;
 
 use crate::RenameOutcome::{Changed, Matched, NotMatched};
 
-use clap::Parser;
-
 #[derive(Parser)]
-#[clap(name = "RustName")]
-#[clap(author = "Twan Stok <twanstok@gmail.com>")]
-#[clap(version = "0.0")]
-#[clap(about = "Mass renaming tool", long_about = "Mass renames files and optionally folders based on regex and template string")]
+#[clap(author, version, about, long_about = "Mass renames files and optionally folders based on regex and template string")]
 struct Args {
+    /// Include subdirectories in the search
+    #[clap(short, long, action)]
+    recursive: bool,
 
+    /// Also rename folders
+    #[clap(short, long, action)]
+    folders: bool,
 
+    /// Print out extra information
+    #[clap(short, long, action)]
+    verbose: bool,
+
+    /// Regex to match filenames against
+    #[clap(value_parser)]
+    regex_str: String,
+
+    /// Template to build new filenames from
+    #[clap(value_parser)]
+    template_str: String,
 }
 
 fn main() {
-    let (regex, template, options) = get_args();
+    let args = Args::parse();
 
-    let mut matched = 0;
-    let mut changed = 0;
-    for file in fs::read_dir(".").unwrap() {
-        if let Ok(file_result) = file {
-            let process_result = rename_file(&regex, &template, file_result);
-            match process_result {
-                Changed => changed += 1,
-                Matched => matched += 1,
-                NotMatched => {}
-            }
-        }
-    }
+    let regex = Regex::new(&args.regex_str).expect("Invalid regex in argument 1");
+
+    let mut matched: u32 = 0;
+    let mut changed: u32 = 0;
+
+    handle_directory(".", &args, &regex, &mut matched, &mut changed);
     println!(
         "{} files matched, {} files renamed",
         matched + changed,
@@ -40,19 +48,23 @@ fn main() {
     );
 }
 
-enum ArgOption {
-    Folders,
-    Recursive
-}
-
-fn get_args() -> (Regex, String, Vec<ArgOption>) {
-    let args: Vec<String> = env::args().collect();
-    assert!(args.len() >= 3, "Wrong number of arguments provided");
-    let options = vec![];
-
-    let regex = Regex::new(args.get(1).unwrap()).expect("Invalid regex in argument 1");
-    let template = args.get(2).unwrap().to_string();
-    (regex, template, options)
+fn handle_directory<P: AsRef<Path>>(path: P, args: &Args, regex: &Regex, matched: &mut u32, changed: &mut u32) {
+    for file_result in fs::read_dir(path).unwrap() {
+        if let Ok(file) = file_result {
+            let is_dir = file.file_type().expect("filetype reading failed").is_dir();
+            if is_dir & args.recursive {
+                handle_directory(file.path(), args, regex, matched, changed);
+            }
+            if !is_dir | (is_dir & args.folders) {
+                let process_result = rename(&regex, &args.template_str, file, args.verbose);
+                match process_result {
+                    Changed => *changed += 1,
+                    Matched => *matched += 1,
+                    NotMatched => {}
+                }
+            }
+        }
+    }
 }
 
 enum RenameOutcome {
@@ -61,7 +73,7 @@ enum RenameOutcome {
     NotMatched,
 }
 
-fn rename_file(regex: &Regex, template: &str, file: DirEntry) -> RenameOutcome {
+fn rename(regex: &Regex, template: &str, file: DirEntry, verbose: bool) -> RenameOutcome {
     let filename = file
         .file_name()
         .to_str()
@@ -70,9 +82,15 @@ fn rename_file(regex: &Regex, template: &str, file: DirEntry) -> RenameOutcome {
     if regex.is_match(&filename) {
         let new_name = generate_new_name(regex, template, &filename);
         if filename != new_name {
+            if verbose {
+                println!("{} -> {}", filename, new_name);
+            }
             fs::rename(filename, new_name).expect("Renaming failed");
             Changed
         } else {
+            if verbose {
+                println!("{} unchanged", filename);
+            }
             Matched
         }
     } else {
